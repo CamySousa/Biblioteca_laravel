79a3b376bcfa10dec3628ae19efd1ff221b3a311
CamySousa <mylle9751@gmail.com>
2022-05-31 11:11:11 -0300

    Funciona

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,26 +3,29 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
         <title>Produtos</title>
     </head>
     <body>
-        <h1>Produtos</h1>
+      
+            <h1>Produtos</h1>
 
-        <form action="/cadastrar-produto" method="POST">
-            @csrf
-            <label for="lblNome:">Nome:</label>
-            <input type="text" name="nome">
-            <br><br>
-            <label for="lblValor">Valor:</label>
-            <input type="text" name="valor">
-            <br><br>
-            <label for="lblQuantidade">Quantidade:</label>
-            <input type="text" name="estoque">
-            <br><br>
-            <label for="lblAutor">Autor:</label>
-            <input type="text" name="autor">
-            <br><br>
-            <button>Cadastrar</button>
-        </form>
+            <form action="/cadastrar-produto" method="POST">
+                @csrf
+                <label for="lblNome:">Nome:</label>
+                <input type="text" name="nome">
+                <br><br>
+                <label for="lblValor">Valor:</label>
+                <input type="text" name="valor">
+                <br><br>
+                <label for="lblQuantidade">Quantidade:</label>
+                <input type="text" name="estoque">
+                <br><br>
+                <label for="lblAutor">Autor:</label>
+                <input type="text" name="autor">
+                <br><br>
+                <button>Cadastrar</button>
+            </form>
+      
     </body>
     </html>
